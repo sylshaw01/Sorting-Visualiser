2a074948453db0f36990578932fd054b25153987
Nathan Shaw <natedogiam@gmail.com>
2020-05-05 17:40:19 +0000

    I finally fixed the performance issues

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
 new Vue({
-	el: "#app",
-	data: {
-		sortingMode: 'BubbleSort',
-		arraySize: 100,
-		sortSpeed: 1,
-		values: [],
-		counter: 0
-	},
-	methods: {
-		scramble: function(){
-			for(let i=0;i<this.arraySize;i++){
-				this.values[i] = Math.random() * 100;
-			}
-			this.reRender();
-			console.log(this.values)
-		},
-		reRender: function(){
-			this.counter++;
-		},
-		async bubbleSort(){
-			for(let i = 0; i < this.arraySize; i++){
-       			for (let j = 0; j < this.arraySize -i; j++) {
-           			await new Promise(r => setTimeout(r, this.sortSpeed));
-           			this.reRender();
-           			this.bubbleExchange(j);
-            	}
+    el: "#app",
+    data: {
+        sortingMode: 'BubbleSort',
+        arraySize: 100,
+        sortSpeed: 1,
+        values: [],
+        counters: []
+    },
+    methods: {
+        scramble: function() {
+            this.values = [];
+            for (let i = 0; i < this.arraySize; i++) {
+                let obj = { 'id': Math.random() * 100 };
+                this.values.push(obj);
+            }
+            console.log(this.values)
+        },
+        reRender: function() {},
+        bubbleSort: function() {
+            for (let i = 0; i < this.arraySize; i++) {
+                for (let j = 0; j < this.arraySize - i - 1; j++) {
+                    this.bubbleExchange(j);
+                }
             }
         },
-        bubbleExchange: function(j){
-        	if (this.values[j] > this.values[j + 1]) {
-               			let tmp = this.values[j];
-               			this.values[j] = this.values[j + 1];
-               			this.values[j + 1] = tmp
-               }
+        bubbleExchange: function(j) {
+            let vm = this;
+            setTimeout(function() {
+                if (vm.values[j].id > vm.values[j + 1].id) {
+                    let tmp = vm.values[j].id;
+                    vm.values[j].id = vm.values[j + 1].id;
+                    vm.values[j + 1].id = tmp
+                }
+            }, this.sortSpeed);
+
         }
     },
-	
-	created() {
-		this.scramble();
-	}
+
+    created() {
+        this.scramble();
+    }
 
 });
