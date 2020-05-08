867edc911583194eebec90399416244072ac9ee6
Nathan Shaw <natedogiam@gmail.com>
2020-05-09 00:18:36 +0100

    finished the first functioning version :)

--- app.js
+++ app.js
@@ -2,41 +2,61 @@ new Vue({
     el: "#app",
     data: {
         sortingMode: 'BubbleSort',
-        arraySize: 100,
-        sortSpeed: 1,
+        arraySize: 50,
+        sortSpeed: 0,
         values: [],
-        counters: []
+        sorting: false
     },
     methods: {
+        //Method to randomise all array values
         scramble: function() {
             this.values = [];
+            this.sorting = false;
             for (let i = 0; i < this.arraySize; i++) {
-                let obj = { 'id': Math.random() * 100 };
+                /*Looks strange but I use an object here so rerendering occurs for individual blocks instead
+                  Of having the whole app rerender on every bubblesort iteration */
+                let obj = {
+                    'id': Math.random() * 100,
+                    'sorted': false
+                };
                 this.values.push(obj);
             }
-            console.log(this.values)
         },
-        reRender: function() {},
-        bubbleSort: function() {
+        //Simple bubble sort algorithm
+        async bubbleSort() {
+            //Setting state variables so sorting cannot be invoked twice at once
+            this.sorting = true;
+            let counter = 0;
             for (let i = 0; i < this.arraySize; i++) {
                 for (let j = 0; j < this.arraySize - i - 1; j++) {
-                    this.bubbleExchange(j);
+                    await this.bubbleExchange(j, i, ++counter);
+
                 }
+                //Changes bar colour to green
+                this.values[this.arraySize - i - 1].sorted = true;
             }
+            //Resetting state variable so you can sort again
+            this.sorting = false;
         },
-        bubbleExchange: function(j) {
+        //Split into separate function here to deal with the async problems that arise with having setTimeout in a loop
+        bubbleExchange: function(j, i, counter) {
             let vm = this;
-            setTimeout(function() {
-                if (vm.values[j].id > vm.values[j + 1].id) {
-                    let tmp = vm.values[j].id;
-                    vm.values[j].id = vm.values[j + 1].id;
-                    vm.values[j + 1].id = tmp
-                }
-            }, this.sortSpeed);
+            if (vm.values[j].id > vm.values[j + 1].id) {
+                let tmp = vm.values[j].id;
+                vm.values[j].id = vm.values[j + 1].id;
+                vm.values[j + 1].id = tmp
+            }
+
+            return new Promise(resolve => setTimeout(resolve, vm.sortSpeed));
 
         }
     },
-
+    computed: {
+        blockWidth: function() {
+            return 100 / this.arraySize + '%';
+        }
+    },
+    //After the instance is initialised, all array elements get randomised
     created() {
         this.scramble();
     }
